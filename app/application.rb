acb976559b6ab302d361735e6a78a9d441345cec
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-05 15:05:29 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application
-
+ 
   def call(env)
     resp = Rack::Response.new
-    
+ 
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
-    
+ 
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
@@ -15,11 +15,9 @@ class Application
       resp.write "You Win"
     else
       resp.write "You Lose"
-     
-    resp.finish
     end
-  
-
+ 
+    resp.finish
   end
-
+ 
 end
